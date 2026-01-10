2526728081d24df6ab06c76134a5a7c331e4469f
alhanaacoran <alhanaacoran@gmail.com>
2026-01-10 08:17:32 +0100

    Update Puppeteer args and executable path

--- src/whatsapp.js
+++ src/whatsapp.js
@@ -15,7 +15,8 @@ function initWhatsApp() {
             }),
             puppeteer: {
                 headless: true,
-               args: ['--no-sandbox', '--disable-setuid-sandbox']
+                executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+                args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
             }
         });
 
